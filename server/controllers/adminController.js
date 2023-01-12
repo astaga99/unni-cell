0bb1077d0f3a5c85b092eac0e5e6ba5cae99240b
unknown <aseprohman99@gmail.com>
2023-01-12 15:43:13 +0700

    setting awal controller

---
+++ server/controllers/adminController.js
@@ -0,0 +1,31 @@
+const { adminModel } = require('../models');
+
+class adminController {
+    static getAllAdmin (req, res) {
+
+    }
+
+    static add (req, res) {
+
+    } 
+
+    static update (req, res) {
+
+    }
+
+    static delete (req, res) {
+
+    }
+
+    static getAdminbyName (req, res) {
+
+    }
+}
+
+
+
+
+
+
+
+module.exports = adminController
---
+++ server/controllers/brandController.js
@@ -0,0 +1,31 @@
+const { brandModel } = require('../models');
+
+class brandController {
+    static getAllbrand (req, res) {
+
+    }
+
+    static add (req, res) {
+
+    } 
+
+    static update (req, res) {
+
+    }
+
+    static delete (req, res) {
+
+    }
+
+    static getBrandbyName (req, res) {
+
+    }
+}
+
+
+
+
+
+
+
+module.exports = brandController
