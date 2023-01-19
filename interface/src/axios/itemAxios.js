f8ceda3ab5ab2bab47a2d65afd189a34860eba14
unknown <aseprohman99@gmail.com>
2023-01-19 22:15:58 +0700

    membuat axios

--- interface/src/axios/itemAxios.js
+++ interface/src/axios/itemAxios.js
@@ -1,11 +1,26 @@
 import axios from "axios";
 import Swal from "sweetalert2";
 
-const URL = "http://localhost:3001/"
+const URL = "http://localhost:3000/api/items"
 
-const getItems = () => {
-
+const getItems = async (cb) => {
+    try { 
+        let items = await axios({
+            method : 'GET',
+            url: URL
+        })
+        cb(items.data)
+    } catch (e) {
+        console.log(e);
+    }
 }
 
+
+
+
+export {
+    getItems,
+
+}
 
 
--- interface/src/components/HomeContent/index.js
+++ interface/src/components/HomeContent/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './styles.css'
+import { getItems } from '../../axios/itemAxios';
 
 const HomeContent = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    getItems(result => console.log(result)) }, [])
+  
+
   return (
-  <>
     <div className="container d-flex justify-content-center mt-50 mb-50">
       <div className="row">
         <div className="col-md-10">
@@ -86,144 +92,21 @@ const HomeContent = () => {
               </div>
 
               <div className="media-body">
-                <h6 className="media-title font-weight-semibold">
-                  <a href="#" data-abc="true">
-                    Apple iPhone XS Max (Gold, 64 GB)
-                  </a>
-                </h6>
-
-                <ul className="list-inline list-inline-dotted mb-3 mb-lg-2">
-                  <li className="list-inline-item">
-                    <a href="#" className="text-muted" data-abc="true">
-                      Phones
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="text-muted" data-abc="true">
-                      Mobiles
-                    </a>
-                  </li>
-                </ul>
-
-                <ul className="list-inline list-inline-dotted mb-0">
-                  <li className="list-inline-item">
-                    All items from{" "}
-                    <a href="#" data-abc="true">
-                      Mobile junction
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    Add to{" "}
-                    <a href="#" data-abc="true">
-                      wishlist
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-              <div className="mt-3 mt-lg-0 ml-lg-3 text-center">
-                <h3 className="mb-0 font-weight-semibold">$612.99</h3>
-
-                <div>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
+                <div className="media-title font-weight-semibold">
+                  <div className='items-items'>
+                    { items.length > 0 ?
+                      items.map(items => {
+                        const {id, name} = items
+                        return (
+                        <ul className='item-item-items'>
+                          <li>{id}</li>
+                          <li>{id}</li>
+                        </ul>
+                        )
+                      }) : <p>loading</p>
+                    }
+                  </div>
                 </div>
-
-                <div className="text-muted">2349 reviews</div>
-
-                <button type="button" className="btn btn-warning mt-4 text-white">
-                  <i className="icon-cart-add mr-2"></i> Add to cart
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card card-body mt-3">
-            <div className="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
-              <div className="mr-2 mb-3 mb-lg-0">
-                <img
-                  src="https://i.imgur.com/5Aqgz7o.jpg"
-                  width="150"
-                  height="150"
-                  alt=""
-                />
-              </div>
-
-              <div className="media-body">
-                <h6 className="media-title font-weight-semibold">
-                  <a href="#" data-abc="true">
-                    Apple iPhone XR (Red, 128 GB)
-                  </a>
-                </h6>
-
-                <ul className="list-inline list-inline-dotted mb-3 mb-lg-2">
-                  <li className="list-inline-item">
-                    <a href="#" className="text-muted" data-abc="true">
-                      Phones
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#" className="text-muted" data-abc="true">
-                      Mobiles
-                    </a>
-                  </li>
-                </ul>    
-
-                <ul className="list-inline list-inline-dotted mb-0">
-                  <li className="list-inline-item">
-                    All items from{" "}
-                    <a href="#" data-abc="true">
-                      Mobile point
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    Add to{" "}
-                    <a href="#" data-abc="true">
-                      wishlist
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-              <div className="mt-3 mt-lg-0 ml-lg-3 text-center">
-                <h3 className="mb-0 font-weight-semibold">$459.99</h3>
-
-                <div>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
-                </div>
-
-                <div className="text-muted">1985 reviews</div>
-
-                <button type="button" className="btn btn-warning mt-4 text-white">
-                  <i className="icon-cart-add mr-2"></i> Add to cart
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card card-body mt-3">
-            <div className="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
-              <div className="mr-2 mb-3 mb-lg-0">
-                <img
-                  src="https://i.imgur.com/Aj0L4Wa.jpg"
-                  width="150"
-                  height="150"
-                  alt=""
-                />
-              </div>
-
-              <div className="media-body">
-                <h6 className="media-title font-weight-semibold">
-                  <a href="#" data-abc="true">
-                    Apple iPhone XS Max (Gold, 64 GB)
-                  </a>
-                </h6>
 
                 <ul className="list-inline list-inline-dotted mb-3 mb-lg-2">
                   <li className="list-inline-item">
@@ -282,7 +165,6 @@ const HomeContent = () => {
         </div>
       </div>
     </div>
-  </>
   );
 }
 
